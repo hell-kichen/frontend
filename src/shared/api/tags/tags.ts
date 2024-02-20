bada606744be55a3bc47e13431074bbe2edd2ab0
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-21 00:29:49 +0300

    added tags api implementation

---
+++ src/shared/api/tags/tags.ts
@@ -0,0 +1,17 @@
+import {Tag} from "./models";
+import {api} from "../base";
+
+const BASE_URL = "/api/tags"
+
+export const getTags = (): Promise<Tag[]> => {
+    return api.get(BASE_URL)
+}
+
+export type GetTagByIDParams = {
+    tagID: string;
+    [x: string]: any;
+}
+
+export const getTagsByID = ({tagID}: GetTagByIDParams): Promise<Tag> => {
+    return api.get(`${BASE_URL}/${tagID}`)
+}
