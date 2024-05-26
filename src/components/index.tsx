3ea32acbbf036e9e6b116071e9ae07778a5a9e0c
sinveria <perevalova_v01@bk.ru>
2024-05-26 21:20:44 +0300

    declared tag and tags-container components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,14 @@ import Card from "./card";
 import Footer from "./footer";
 import Header from "./header";
 import Nav from "./nav";
+import Tag from "./tag";
+import TagsContainer from "./tags-container";
 
 export {
     Card,
     Footer,
     Header,
-    Nav
+    Nav,
+    Tag,
+    TagsContainer
 }
