02753e0534cb2d6e67e8a6dd87bbacab7273f262
Getlles <attencrow@gmail.com>
2024-05-28 13:38:38 +0300

    added form SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import MainPage from "./main"
 import SingleCard from "./single-card";
 import SignUp from "./signup";
+import SignIn from "./signin";
 
 export {
     MainPage,
     SingleCard,
-    SignUp
+    SignUp,
+    SignIn
 }
